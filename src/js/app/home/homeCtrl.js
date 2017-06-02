a05b78ec84337f0b8da192f68b6a68700233f5c6
sylvia_mavuba <smavuba@voyages-sncf.com>
2017-06-02 18:10:00 +0200

    loop on all events

--- src/js/app/home/homeCtrl.js
+++ src/js/app/home/homeCtrl.js
@@ -1,83 +1,153 @@
 //var loadResult = require('./getRestaurants').getRestaurant;
 var moment          = require('moment');
 var $               = require('jquery');
+var _ = require('underscore');
 
 module.exports = function ($scope, $http) {
 
 
     function timer(settings){
         //debugger
+        var endDate = {
+            wedding: '2017-06-24 16:00:00',
+            plane: '2017-06-24 16:00:00',
+            holidays: '2017-06-26 9:30:00',
+        };
+
         var config = {
-            endDate: '2017-06-24 16:00:00',
-            planeDate: '2017-06-24 16:00:00',
-            maldivesDate: '2017-06-26 9:30:00',
+            // endDate: '2017-06-24 16:00:00',
+            // planeDate: '2017-06-24 16:00:00',
+            // maldivesDate: '2017-06-26 9:30:00',
             timeZone: 'Europe/Dublin',
-            hours: $('#hours'),
-            minutes: $('#minutes'),
-            seconds: $('#seconds'),
+            // hours: $('#hours'),
+            // minutes: $('#minutes'),
+            // seconds: $('#seconds'),
             newSubMessage: 'and should be back online in a few minutes...'
         };
 
-        //Je m'assure d'avoir un 0 avant les chiffres
-        function prependZero(number){
-            return number < 10 ? '0' + number : number;
-        }
-
-        $.extend(true, config, settings || {});
-
-        var currentTime = moment();
-        //Wedding
-        var endDate = moment.tz(config.endDate, config.timeZone);
-        var diffTime = endDate.valueOf() - currentTime.valueOf();  //valueOf() nombre de milliseconds écoulée
-        var duration = moment.duration(diffTime, 'milliseconds');
-        var days = duration.days();
-        var interval = 1000;
-        var subMessage = $('.sub-message');
-        var clock = $('.clock');
-
-        //Plane
-        var departure = moment.tz(config.planeDate, config.timeZone);
-        var departureDiffTime = departure.valueOf() - currentTime.valueOf();
-        var durationPlane = moment.duration(departureDiffTime, 'milliseconds');
-
-        //Maldives
-        var arrival = moment.tz(config.maldivesDate, config.timeZone);
-        var arrivalDiffTime = arrival.valueOf() - currentTime.valueOf();
-        var durationMaldives = moment.duration(arrivalDiffTime, 'milliseconds');
+        var dateEvent = '';
 
 
 
-        if(diffTime < 0){
-            endEvent(subMessage, config.newSubMessage, clock);
-            return;
-        }
+        // var getEndDateEvent = function (endDate) {
+        //     var config = {
+        //         endDate: endDate
+        //     }
+        // }
+        //
+        // getEndDateEvent(config.endDate); //wedding
+        // getEndDateEvent(config.planeDate); // plane
+
         //debugger
 
-        if(days > 0){
-            $('#days').text(prependZero(days));
-            //$('.days').css('display', 'inline-block');
-        }
+        //Je m'assure d'avoir un 0 avant les chiffres
 
 
-        var intervalID = setInterval(function(){
-            duration = moment.duration(duration - interval, 'milliseconds');
-            var hours = duration.hours(),
-                minutes = duration.minutes(),
-                seconds = duration.seconds();
-            days = duration.days();
-            if(hours  <= 0 && minutes <= 0 && seconds  <= 0 && days <= 0){
-                clearInterval(intervalID);
-                endEvent(subMessage, config.newSubMessage, clock);
-                window.location.reload();
+
+
+
+        $.extend(true, config, settings, endDate || {});
+
+
+
+
+
+        var renderEvent = function(config, settings, endDate) {
+            var endDate = {
+                wedding: '2017-06-24 16:00:00',
+                plane: '2017-06-24 16:00:00',
+                holidays: '2017-06-26 9:30:00',
+            };
+
+            var config = {
+                // endDate: '2017-06-24 16:00:00',
+                // planeDate: '2017-06-24 16:00:00',
+                // maldivesDate: '2017-06-26 9:30:00',
+                timeZone: 'Europe/Dublin',
+                // hours: $('#hours'),
+                // minutes: $('#minutes'),
+                // seconds: $('#seconds'),
+                newSubMessage: 'and should be back online in a few minutes...'
+            };
+
+            var dateEvent = '';
+            var endDate = {
+                wedding: '2017-06-24 16:00:00',
+                plane: '2017-06-24 16:00:00',
+                holidays: '2017-06-26 9:30:00',
+            };
+
+            debugger
+            var arr = _.toArray(endDate);
+
+            for(var i=0; i < arr.length; i++) {
+
+                function prependZero(number){
+                    return number < 10 ? '0' + number : number;
+                }
+
+                var dateEvent = arr[i];
+                var currentTime = moment();
+
+                var endDate = moment.tz(dateEvent, config.timeZone);
+                var diffTime = endDate.valueOf() - currentTime.valueOf();  //valueOf() nombre de milliseconds écoulée
+                var duration = moment.duration(diffTime, 'milliseconds');
+                var days = duration.days();
+                var interval = 1000;
+                var subMessage = $('.sub-message');
+                var clock = $('.clock');
+
+
+
+
+
+                if(diffTime < 0){
+                    endEvent(subMessage, config.newSubMessage, clock);
+                    return;
+                }
+                //debugger
+
+                if(days > 0){
+                    $('#days').text(prependZero(days));
+                    //$('.days').css('display', 'inline-block');
+                }
+
+                var intervalID = setInterval(function(){
+                    duration = moment.duration(duration - interval, 'milliseconds');
+                    var hours = duration.hours(),
+                        minutes = duration.minutes(),
+                        seconds = duration.seconds();
+                        debugger
+                    days = duration.days();
+                    if(hours  <= 0 && minutes <= 0 && seconds  <= 0 && days <= 0){
+                        clearInterval(intervalID);
+                        endEvent(subMessage, config.newSubMessage, clock);
+                        window.location.reload();
+                    }
+                    if(days === 0){
+                        $('.days').hide();
+                    }
+
+                    var space = '';
+
+                    space += '<div id="days">'+ (prependZero(days)) +'</div>'
+                    +'<div id="hours">'+(prependZero(hours))+'</div>'
+                    +'<div id="minutes">'+(prependZero(minutes))+'</div>'
+                    +'<div id="seconds">'+(prependZero(seconds))+'</div>';
+                    $('.toto').append(space);
+                    // $('#days').text(prependZero(days));
+                    // config.hours.text(prependZero(hours));
+                    //
+                    // config.minutes.text(prependZero(minutes));
+                    // config.seconds.text(prependZero(seconds));
+                }, interval);
             }
-            if(days === 0){
-                $('.days').hide();
-            }
-            $('#days').text(prependZero(days));
-            config.hours.text(prependZero(hours));
-            config.minutes.text(prependZero(minutes));
-            config.seconds.text(prependZero(seconds));
-        }, interval);
+        };
+        //renderEvent();
+
+        console.log(dateEvent);
+
+        debugger
     }
 
     function endEvent($el, newText, hideEl){
